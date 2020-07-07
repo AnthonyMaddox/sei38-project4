b8206514c7b953f5410f226f74961ed6b41c3af0
AnthonyMaddox <anthonyjmaddox@gmail.com>
2020-07-07 19:58:37 -0400

    added two divs that can switch between being displayed

--- script.js
+++ script.js
@@ -1,27 +1,41 @@
 //start building some things on the page with js so you can feel having access to them
-
-
+//pics from https://www.webmd.com/skin-problems-and-treatments/ss/slideshow-poison-plants-guide
+//let cardDiv = document.querySelector(".cardDiv");
+let newCard;
+let cardArray;
 console.log("hello world");
 let prompter = document.querySelector(".prompter");
 prompter.innerHTML = "Please Reset the Game";
 prompter.classList.add("green");
+
 let cardDiv = document.querySelector(".cardDiv");
-cardDiv.addEventListener('click', flipCard);
-let input = document.querySelector("input");
+cardDiv.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (bottomCard.classList.contains("hideCard")) {
+    bottomCard.classList.remove("hideCard");
+    topCard.classList.add("hideCard");
+  } else {
+    topCard.classList.remove("hideCard");
+    bottomCard.classList.add("hideCard");
+  }
+});
 
-let form = document.querySelector("form")
+let topCard = document.querySelector(".topCard");
+let bottomCard = document.querySelector(".bottomCard");
+bottomCard.classList.add("hideCard");
 
-form.addEventListener("keydown", flipCard);
-form.addEventListener("keyup", checkCard);
-
-let newCard;
-let cardArray;
 function checkCard() {
-   console.log('checkCard function')
+  console.log("checkCard function");
 }
 function flipCard() {
-   console.log('flipCard function')
+  if (bottomCard.classList.contains("hideCard")) {
+    bottomCard.classList.remove("hideCard");
+    topCard.classList.add("hideCard");
+  }
 }
-function compareCards(){
-   console.log('compareCards function')
+function compareCards() {
+  console.log("compareCards function");
 }
+//let form = document.querySelector('form')
+//form.addEventListener("keyup", checkCard);
+//form.addEventListener("submit", flipCard);
