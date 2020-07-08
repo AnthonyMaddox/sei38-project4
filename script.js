c75cea5a0f602f671246ca76301fdb3e2dc9668d
AnthonyMaddox <anthonyjmaddox@gmail.com>
2020-07-07 21:05:15 -0400

    started building deck

--- script.js
+++ script.js
@@ -1,8 +1,44 @@
 //start building some things on the page with js so you can feel having access to them
 //pics from https://www.webmd.com/skin-problems-and-treatments/ss/slideshow-poison-plants-guide
 //let cardDiv = document.querySelector(".cardDiv");
-let newCard;
-let cardArray;
+
+let cardArray = [];
+class Flashcard {
+  constructor(name, img) {
+    this.name = name;
+    this.img = img;
+  }
+}
+const ivy = new Flashcard("Poison Ivy", "imgs/ivy.jpg");
+const oak = new Flashcard("Poison Oak", "imgs/oak.jpg");
+const sumac = new Flashcard("Poison Sumac", "imgs/sumac.jpg");
+const foxglove = new Flashcard("Foxgolve", "imgs/foxglove.jpg");
+const jimsonweed = new Flashcard("Jimsonweed", "imgs/jimsonweed.jpg");
+const hemlock = new Flashcard("Hemlock", "imgs/ivy.jpg");
+const oleander = new Flashcard("Oleander", "imgs/oleander.jpg");
+const parnsnip = new Flashcard("Parnsnip", "iimgs/parnsnip.jpg");
+const nettle = new Flashcard("Stinging Nettle", "imgs/nettle.jpg");
+const nightshade = new Flashcard("Nightshade", "imgs/nightshade.jpg");
+const azalea = new Flashcard("Azalea", "imgs/azalea.jpg");
+const mistletoe = new Flashcard("Mistletoe", "imgs/mistletoe.jpg");
+const hogweed = new Flashcard("Hogweed", "imgs/hogweed.jpg");
+cardArray.push(
+  ivy,
+  oak,
+  sumac,
+  foxglove,
+  jimsonweed,
+  hemlock,
+  oleander,
+  parnsnip,
+  nettle,
+  nightshade,
+  azalea,
+  mistletoe,
+  hogweed
+);
+console.log(cardArray);
+
 console.log("hello world");
 let prompter = document.querySelector(".prompter");
 prompter.innerHTML = "Please Reset the Game";
@@ -24,14 +60,8 @@ let topCard = document.querySelector(".topCard");
 let bottomCard = document.querySelector(".bottomCard");
 bottomCard.classList.add("hideCard");
 
-function checkCard() {
-  console.log("checkCard function");
-}
 function flipCard() {
-  if (bottomCard.classList.contains("hideCard")) {
-    bottomCard.classList.remove("hideCard");
-    topCard.classList.add("hideCard");
-  }
+  //at card div eventlistener
 }
 function compareCards() {
   console.log("compareCards function");
@@ -39,3 +69,4 @@ function compareCards() {
 //let form = document.querySelector('form')
 //form.addEventListener("keyup", checkCard);
 //form.addEventListener("submit", flipCard);
+//let newCard;
